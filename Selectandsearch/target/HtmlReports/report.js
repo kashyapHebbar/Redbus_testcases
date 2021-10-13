13323eeffa8e9cdfd72f3dffead1cd095d8f9da9
kashyapHebbar <65105317+kashyapHebbar@users.noreply.github.com>
2021-10-13 16:15:21 +0530

    Add files via upload

---
+++ Selectandsearch/target/HtmlReports/report.js
@@ -0,0 +1,336 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/Features/Button.feature");
+formatter.feature({
+  "name": "To Test search button module functionality",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "To verify user can enter from location",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "user is on Button Homepage",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefination.button.user_is_on_button_homepage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user clicks on the fromtextbox",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefination.button.user_clicks_on_the_fromtextbox()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "he should enter the From place details",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefination.button.he_should_enter_the_from_place_details()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user clicks on the Totextbox",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefination.button.user_clicks_on_the_totextbox()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "he should enter the to place details",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefination.button.he_should_enter_the_to_place_details()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "choose the date",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefination.button.choose_the_date()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click on search button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefination.button.click_on_search_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.uri("file:src/test/resources/Features/Sorting.feature");
+formatter.feature({
+  "name": "To Test Sorting module functionality",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "To verify user can sort location",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "user is on sorting Homepage",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefination.Sort.user_is_on_sorting_homepage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user clicks on the Fromtextbox",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefination.Sort.user_clicks_on_the_fromtextbox()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "he should enter the Fromplace details",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefination.Sort.he_should_enter_the_fromplace_details()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user clicks on the To textbox",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefination.Sort.user_clicks_on_the_to_textbox()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "he should enter the Toplace details",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefination.Sort.he_should_enter_the_toplace_details()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "choose the date of travel",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefination.Sort.choose_the_date_of_travel()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click on search Button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefination.Sort.click_on_search_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click on departure sort",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefination.Sort.click_on_departure_sort()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Then click on the duration sort",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefination.Sort.then_click_on_the_duration_sort()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on the Arrival sort",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefination.Sort.click_on_the_arrival_sort()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Then click on Ratings sort",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefination.Sort.then_click_on_ratings_sort()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click on Fare sort",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefination.Sort.click_on_fare_sort()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Then click on Seatsavailable sort",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefination.Sort.then_click_on_seatsavailable_sort()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.uri("file:src/test/resources/Features/To.feature");
+formatter.feature({
+  "name": "To Test To module functionality",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "To verify user can enter To location",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "user is on To Homepage",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefination.To.user_is_on_to_homepage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user clicks on Totextbox",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefination.To.user_clicks_on_totextbox()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "he should enter the To details",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefination.To.he_should_enter_the_to_details()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.uri("file:src/test/resources/Features/from.feature");
+formatter.feature({
+  "name": "To Test From module functionality",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "To verify user can enter from location",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "user is on  From Homepage",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefination.From.user_is_on_from_homepage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user clicks on fromtextbox",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefination.From.user_clicks_on_fromtextbox()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "he should enter the from details",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefination.From.he_should_enter_the_from_details()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.uri("file:src/test/resources/Features/onward.feature");
+formatter.feature({
+  "name": "To Test Date module functionality",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "To verify user can enter The Date",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "user is on onward Homepage",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefination.Date.user_is_on_onward_homepage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "he should pick the To Date of travel",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefination.Date.he_should_pick_the_to_date_of_travel()"
+});
+formatter.result({
+  "status": "passed"
+});
+});
